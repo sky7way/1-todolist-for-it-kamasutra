2c70e64c16fe9f1b0ee31231d7b4d65106affb5f
Aleksandr <sky7way@yandex.ru>
2023-05-23 17:02:25 +0300

    add arrays

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,17 @@ function App() {
     const title2 = 'What to learn 2';
     const title3 = 'What to learn 3';
 
+    const task1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+    ]
+    const task2 = [
+        {id: 1, title: "Hello world!", isDone: true},
+        {id: 2, title: "I'm happy!", isDone: false},
+        {id: 3, title: "Yo!", isDone: true},
+    ]
+
     return (
         <div className="App">
             <Todolist title={ title1 } />
